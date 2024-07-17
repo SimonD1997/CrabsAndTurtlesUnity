32609e32f53e0cfb4cfd496b3849b205bae254ca
Simon Doubleday Laptop <simon@doubleday.de>
2024-07-17 15:54:03 +0200

    Variablesheet2
    Update

--- Assets/Scenes/Gameboard/Scripts/VariablenTafel.cs
+++ Assets/Scenes/Gameboard/Scripts/VariablenTafel.cs
@@ -21,14 +21,17 @@ namespace Scenes.Gameboard.Scripts
         public GameObject rotGameObject;
         public GameObject blauGameObject;
         public GameObject gelbGameObject;
-        
+        public GameObject logTextField;
         
         
         // Start is called before the first frame update
         void Start()
         {
             _gruenVarList = new List<int>();
-            
+            _rotVarList = new List<int>();
+            _blauVarList = new List<int>();
+            _gelbVarList = new List<int>();
+
             //_gruen[0] = 
 
         }
@@ -39,25 +42,44 @@ namespace Scenes.Gameboard.Scripts
         
         }
 
+        // gibt nach jedem Input alle Variablen aller Spalten in neu in die Liste ein um 
+        // um ein nachvollziehbaren log zu erhalten...
+        
         public void Gruen()
         {
             _aktuelleVarGruen = Convert.ToInt32(gruenGameObject.GetComponentInChildren<TMP_InputField>().text);
-            _gruenVarList.Add(_aktuelleVarGruen);
+            addToList();
+            gruenGameObject.GetComponentInChildren<TextMeshProUGUI>().text = _aktuelleVarGruen.ToString();
+            //eingabe erfolgreich und an den Gamecontroller zurückgeben...
         }
         
         public void Rot()
         {
             _rotVarList.Add(Convert.ToInt32(rotGameObject.GetComponentInChildren<TextMeshPro>().text));
+            addToList();
+            rotGameObject.GetComponentInChildren<TextMeshProUGUI>().text = _aktuelleVarRot.ToString();
         }
         
         public void Blau()
         {
             _blauVarList.Add(Convert.ToInt32(blauGameObject.GetComponentInChildren<TextMeshPro>().text));
+            addToList();
+            blauGameObject.GetComponentInChildren<TextMeshProUGUI>().text = _aktuelleVarBlau.ToString();
         }
         
         public void Gelb()
         {
             _gelbVarList.Add(Convert.ToInt32(gelbGameObject.GetComponentInChildren<TMP_InputField>().text));
+            addToList();
+            gelbGameObject.GetComponentInChildren<TextMeshProUGUI>().text = _aktuelleVarGelb.ToString();
+        }
+
+        void addToList()
+        {
+            _gruenVarList.Add(_aktuelleVarGruen);
+            _rotVarList.Add(_aktuelleVarRot);
+            _blauVarList.Add(_aktuelleVarBlau);
+            _gelbVarList.Add(_aktuelleVarGelb);
             
         }
 
@@ -66,7 +88,21 @@ namespace Scenes.Gameboard.Scripts
         /// </summary>
         public void onShow()
         {
-            gruenGameObject.GetComponentInChildren<TextMeshProUGUI>().text = _aktuelleVarGruen.ToString();
+            String stringtemp = "";
+            TextMeshProUGUI logTemp = logTextField.GetComponent<TextMeshProUGUI>();
+            for (int i = 0; i < _gruenVarList.Count; i++)
+            {
+                logTemp.text.Insert(logTemp.text.Length, _aktuelleVarGruen.ToString());
+                stringtemp = stringtemp +  _gruenVarList[i].ToString() + "\t" +_rotVarList[i].ToString() +
+                "\t" + _rotVarList[i].ToString() + "\t" + _gelbVarList[i].ToString() + System.Environment.NewLine;
+                
+            }
+            
+            
+            logTemp.text = stringtemp;
+            
+            
+
         }
         
         
